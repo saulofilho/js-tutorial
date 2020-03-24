37823d93555b132d6d2cc997ef535a407a58a719
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-03-23 22:30:08 -0300

    chapter 3

---
+++ eloquent-js/chap-3/c-3-1.js
@@ -0,0 +1,21 @@
+let minNumber = (a, b) => {
+  if (Math.min(a, b) == 0) return a;
+  else return b;
+}
+
+console.log(minNumber(0, 10));
+// → 0
+console.log(minNumber(0, -10));
+// → -10
+
+function min(a, b) {
+  if (a < b)
+    return a;
+  else
+    return b;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
